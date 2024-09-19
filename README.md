3648b590bb712bf5ea474894eec45768119cf7d6
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-19 10:40:45 +0200

    Added 121. Best Time to Buy and Sell Stock

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 -   IsPalindrome
 -   Valid Palindrome
 -   Binary Search
+-   121. Best Time to Buy and Sell Stock
--- main.cpp
+++ main.cpp
@@ -3,6 +3,51 @@
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <cmath>
+
+// ====================================================
+// 121. Best Time to Buy and Sell Stock
+// ====================================================
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+//
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// 
+// Example 2:
+// 
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+int max_profit(std::vector<int>& prices){
+    int pointerA = prices[0];
+    int pointerB = 0;
+    for(int i = 0; i < prices.size(); i++){
+        if(pointerA > prices[i]){
+            pointerA = prices[i];
+        }
+        pointerB = std::max(pointerB, prices[i] - pointerA);
+    }
+    return pointerB;
+}
+
+void problem_buy_stock(){
+    std::vector<int> inputA = {7, 1, 5, 3, 6, 4};
+    std::vector<int> inputB = {7, 6, 4, 3, 1};
+
+    int resultA = max_profit(inputA);
+    int resultB = max_profit(inputB);
+
+    std::cout << "result a: " << resultA << std::endl;
+    std::cout << "result b: " << resultB << std::endl;
+}
 
 // BINARY SEARCH
 
@@ -88,31 +133,32 @@ std::vector<int> two_sum(std::vector<int>& nums, int target){
 
 int main(){
     // TWO SUM
-    std::cout << "TWO SUM" << std::endl;
-    std::vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    std::vector<int> result = two_sum(nums, target);
-    std::cout << result[0] << ", " << result[1] << std::endl;
-    std::cout << "IS PALINDROME" << std::endl;
-    std::string s = "A man, a plan, a canal: Panama";
-    std::string s1 = "race a car";
-    bool isp = isPalindrome(s); 
-    bool isp1 = isPalindrome(s1); 
-    std::cout << "is palindrome: " << isp << std::endl;
-    std::cout << "is palindrome: " << isp1 << std::endl;
-
-    std::cout << "Validate Parentheses" << std::endl;
-   
-    std::string a = "[]"; 
-    std::string a1 = "([{}])"; 
-    std::string a2 = "[(])"; 
-
-    isValid(a);
-    isValid(a1);
-    isValid(a2);
-
-    problem_binary_search();
-
+//    std::cout << "TWO SUM" << std::endl;
+//    std::vector<int> nums = {2, 7, 11, 15};
+//    int target = 9;
+//    std::vector<int> result = two_sum(nums, target);
+//    std::cout << result[0] << ", " << result[1] << std::endl;
+//    std::cout << "IS PALINDROME" << std::endl;
+//    std::string s = "A man, a plan, a canal: Panama";
+//    std::string s1 = "race a car";
+//    bool isp = isPalindrome(s); 
+//    bool isp1 = isPalindrome(s1); 
+//    std::cout << "is palindrome: " << isp << std::endl;
+//    std::cout << "is palindrome: " << isp1 << std::endl;
+//
+//    std::cout << "Validate Parentheses" << std::endl;
+//   
+//    std::string a = "[]"; 
+//    std::string a1 = "([{}])"; 
+//    std::string a2 = "[(])"; 
+//
+//    isValid(a);
+//    isValid(a1);
+//    isValid(a2);
+//
+//    problem_binary_search();
+    
+    problem_buy_stock();
     return 0;
 
 
