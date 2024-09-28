e95c632e9f2866214d649c721a735d0fe4bb1638
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-28 11:28:15 +0200

    Updated solution to 543

--- problems/543_diamter_of_binary_tree/main.cpp
+++ problems/543_diamter_of_binary_tree/main.cpp
@@ -4,26 +4,35 @@
 
 class Solution{
     public:
-      	
-
+        int result = 0;
+        int diameterOfBinaryTree(TreeNode* root) {
+            dfs(root);
+            return result;
+        }
+                
+        int dfs(TreeNode* current){
+            if(current == nullptr) return 0;
+            int left = dfs(current->left);
+            int right = dfs(current->right);
+            result = std::max(result, left + right);
+            return 1 + std::max(left, right);
+        }
 };
 
 int main(){
-    Solution* solution = new Solution();
+    Solution solution;
 
     BinaryTree* binary_tree = new BinaryTree(1);
     TreeNode* two = new TreeNode(2);
     TreeNode* three = new TreeNode(3);
     TreeNode* four = new TreeNode(4);
     TreeNode* five = new TreeNode(5);
-
     binary_tree->root->left = two;
     binary_tree->root->right = three;
-
     two->left = four;
     two->right = five;
-    binary_tree->traverse(TraversalType::IN_ORDER);
 
-    int diameter = solution->diameterOfBinaryTree(binary_tree->root);
+    int diameter = solution.diameterOfBinaryTree(binary_tree->root);
+    std::cout << "diameter: "<< diameter << std::endl;
     return 0;
 }
