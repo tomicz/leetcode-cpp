94e089be4442fd9d291fd3ff44daca04fdc250d9
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-30 03:33:47 +0200

    Added 100 Same Tree problem with solution

--- problems/100_same_tree/main.cpp
+++ problems/100_same_tree/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <stack>
 #include "../../includes/binary_tree.h"
 
 class Solution{
     public:
         bool is_same_tree(TreeNode* p, TreeNode* q){
-                    
+            std::stack<TreeNode*> a_stack;
+            std::stack<TreeNode*> b_stack;
+            a_stack.push(p);
+            b_stack.push(q);
+            while(a_stack.size() > 0 && b_stack.size() > 0){
+                TreeNode* a = a_stack.top();     
+                TreeNode* b = b_stack.top();     
+                a_stack.pop(); 
+                b_stack.pop();
+                if(a == nullptr && b == nullptr) continue;
+                if(a == nullptr || b == nullptr || a->value != b->value) return false;
+                a_stack.push(a->left);
+                a_stack.push(a->right);
+
+                b_stack.push(b->left);
+                b_stack.push(b->right);
+            }
+
             return true;
         }
 };
@@ -18,13 +36,13 @@ int main(){
     TreeNode* p2 = new TreeNode(3);
     p->left = p1;
     p->right = p2;
-    binary_tree->traverse(TraversalType::IN_ORDER);
     TreeNode* q = binary_tree1->root;
     TreeNode* q1 = new TreeNode(2);
     TreeNode* q2 = new TreeNode(3);
     q->left = q1;
     q->right = q2;
-    binary_tree1->traverse(TraversalType::IN_ORDER);
     bool is_same = solution.is_same_tree(p, q);
+    std::cout << std::boolalpha;
+    std::cout << "is same tree: " << is_same << std::endl;
     return 0;
 }
