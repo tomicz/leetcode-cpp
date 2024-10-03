5bf5bc0a9f79ead4b931551220f715897c34954c
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-10-04 00:42:27 +0200

    Added working solution to valid parentheses

--- problems/20_valid_parentheses/main.cpp
+++ problems/20_valid_parentheses/main.cpp
@@ -4,6 +4,21 @@
 class Solution{
     public:
         bool isValid(std::string s){
+            std::stack<char> s_stack;
+            if(s.size() % 2 != 0) return false;
+            for(char c: s){
+                if(c == '(' || c == '[' || c == '{')
+                    s_stack.push(c);
+                else if(!s_stack.empty() && s_stack.top() == '(' && c == ')')
+                    s_stack.pop();
+                else if(!s_stack.empty() && s_stack.top() == '[' && c == ']')
+                    s_stack.pop();
+                else if(!s_stack.empty() && s_stack.top() == '{' && c == '}')
+                    s_stack.pop();
+                else
+                    return false;
+            }
+            if(s_stack.size() == 0) return true;
             return false;
         }
 };
@@ -14,5 +29,9 @@ int main(){
     bool is_valid = solution.isValid(s);
     std::cout << std::boolalpha;
     std::cout << "is valid: " << is_valid << std::endl;
+    std::string s1 = "(]";
+    bool is_valid1 = solution.isValid(s1);
+    std::cout << std::boolalpha;
+    std::cout << "is valid1: " << is_valid1 << std::endl;
     return 0;
 }
