1e0ea03c38e6ab67bd23b3e95276d0d6cbf5b0f8
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-10-06 02:49:31 +0200

    Added new problem 347 top k frequent elements without solution

---
+++ problems/347_top_k_frequent_elements/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution{
+    public:
+        std::vector<int> topKFrequent(std::vector<int>& nums, int k){
+            
+        }
+};
+
+int main(){
+    Solution solution;
+    int k = 2;
+    std::vector<int> nums = {1,1,1,2,2,3};
+    std::vector top_k_elements = solution.topKFrequent(nums, k);
+    return 0;
+}
--- problems/49_group_anagrams/main.cpp
+++ problems/49_group_anagrams/main.cpp
@@ -31,7 +31,6 @@ class Solution{
                 anagrams.push_back(i.second);
             }
 
-
             return anagrams;
         }
 };
