ed4649681981ace709e1a28215d987002ee47fcb
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-10-10 10:45:01 +0200

    Added 3Sum solution

--- problems/15_3Sum/main.cpp
+++ problems/15_3Sum/main.cpp
@@ -4,7 +4,7 @@
 class Solution {
     void sort(std::vector<int>& nums){
         for(size_t i = 1; i < nums.size(); i++){
-            size_t j = i - 1;
+            int j = i - 1;
             int temp = nums[i];
             while(j>=0 && nums[j] > temp){
                 nums[j+1] = nums[j];
@@ -14,10 +14,31 @@ class Solution {
         }
     }
     public:
-        std::vector<std::vector<int>> threeSum(std::vector<int> nums) {
-            std::vector<std::vector<int>> triplets;
-            
-            return triplets;
+        std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+            std::vector<std::vector<int>> v;
+            if(nums.size() < 3) return {};
+            sort(nums);
+            for(size_t i = 0; i < nums.size(); i++){
+                if(i > 0 && nums[i] == nums[i - 1]) continue;
+                int l = i + 1;
+                int r = nums.size() -1;
+                while(l < r){
+                    int sum = nums[i] + nums[l] + nums[r];
+                    if(sum > 0){
+                        r--;
+                    }
+                    else if(sum < 0){
+                        l++;
+                    }else{
+                        v.push_back({nums[i], nums[l], nums[r]});
+                        l++;
+                        while(nums[l] == nums[l-1] && l < r){
+                            l++;
+                        }
+                    }
+                }
+            }
+            return v;
         }
 };
 
