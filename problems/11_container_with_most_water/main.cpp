a0bc6fdfb8e166d046e6138269362b6324ebf6a2
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-10-12 12:20:30 +0200

    Added solution 11. Container With Most Water

--- problems/11_container_with_most_water/main.cpp
+++ problems/11_container_with_most_water/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <vector>
 
 class Solution{
     public:
-        int maxArea(std::vector<int>& height){
-            return 0;
+        int maxArea(std::vector<int>& heights){
+            int max_area = 0;
+            int l = 0;
+            int r = heights.size()-1;
+            while(l < r){
+                int area = (r - l) * std::min(heights[l], heights[r]);
+                max_area = std::max(max_area, area);
+                if(heights[l] < heights[r])
+                    l++;
+                else
+                    r--;
+            }
+            return max_area;
         }
 };
 
 int main(){
-
+    Solution solution;
+    std::vector<int> heights = {1,8,6,2,5,4,8,3,7};
+    int max_area = solution.maxArea(heights);
+    std::cout << "Max area: " << max_area << std::endl;
     return 0;
 }
